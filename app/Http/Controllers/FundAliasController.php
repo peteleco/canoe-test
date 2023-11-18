5c86bd0fc3c4ca8cea1b10d09f3fb5fd1c509b06
Leonardo <leonardo@odeliverypro.com>
2023-11-17 21:15:28 -0300

    make Fund Alias files

---
+++ app/Http/Controllers/FundAliasController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FundAlias;
+use Illuminate\Http\Request;
+
+class FundAliasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FundAlias $fundAlias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FundAlias $fundAlias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FundAlias $fundAlias)
+    {
+        //
+    }
+}
