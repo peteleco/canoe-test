5f08365f512b126e3c9019d2779f17912e880a1f
Leonardo <leonardo@odeliverypro.com>
2023-11-17 21:14:49 -0300

    make Fund files

---
+++ app/Http/Controllers/FundController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fund;
+use Illuminate\Http\Request;
+
+class FundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fund $fund)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Fund $fund)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fund $fund)
+    {
+        //
+    }
+}
