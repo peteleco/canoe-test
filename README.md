64ba59ecdadf050afef578fccf182fb88ea5191a
Leonardo <leonardo@odeliverypro.com>
2023-11-17 22:50:11 -0300

    add some seed data

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ cd canoe-test && ./vendor/bin/sail up
 # To start all of the Docker containers in the background, you may start Sail in "detached" mode:
 ./vendor/bin/sail up -d
 ```
+
+### Migrate the database with some values
+```shell
+sail artisan migrate --step --seed
+# or make a full refresh
+sail artisan migrate:refresh --seed
+```
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Fund;
+use App\Models\FundAlias;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Fund::factory()
+            ->has(FundAlias::factory()->count(5), 'aliases')
+            ->has(Company::factory()->count(5), 'companies')
+            ->count(20)
+            ->create();
     }
 }
